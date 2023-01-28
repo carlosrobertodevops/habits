1e08cc116c8c885c54e8fb551f20256189593ed5
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2023-01-28 19:24:53 -0300

    Web - Popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,19 @@
+import * as Popover from '@radix-ui/react-popover'
+
 // interface HabitDayProps {
 //   completed: number
 // }
 
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px]">
+          <div className="p-6 w-full"></div>
+
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
