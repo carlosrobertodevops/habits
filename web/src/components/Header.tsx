db5e3707ad4070674607074bc10489b44ce8dd8e
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2023-01-28 10:50:23 -0300

    Web - Ajustes

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -22,7 +22,7 @@ export function Header() {
   const [state, setState] = React.useState<State>({
     open: false,
     vertical: 'top',
-    horizontal: 'left',
+    horizontal: 'center',
   });
 
   const { open, vertical, horizontal } = state;
@@ -47,23 +47,28 @@ export function Header() {
 
       <button
         type="button"
-        onClick={buttonClick({vertical: 'top',horizontal: 'right'})}
+        onClick={buttonClick({vertical: 'top', horizontal: 'center'})}
         className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300">
         <Plus size={20} className="text-violet-500"/>
         Novo hábito
       </button>
+
+      {/* snackbar */}
       <Snackbar
         anchorOrigin={{ vertical, horizontal }}
         open={open}
         autoHideDuration={1000}
         onClose={buttonClose}>
+
+        {/* alerta de sucesso */}
         <Alert
           onClose={buttonClose}
           severity="success"
           sx={{ width: '100%' }}>
-          Vc Clicou!
+          Clicado com sucessi !
         </Alert>
-    </Snackbar>
+
+      </Snackbar>
     </div >
   )
 }
