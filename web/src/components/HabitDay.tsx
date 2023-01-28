5f0d165f150b6de18910abc9541e173cc22a8e21
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2023-01-28 20:03:06 -0300

    Web - Popover do HabitDay

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -9,9 +9,13 @@ export function HabitDay() {
     <Popover.Root>
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
       <Popover.Portal>
-        <Popover.Content className="min-w-[320px]">
-          <div className="p-6 w-full"></div>
-
+        <Popover.Content className="p-6 min-w-[320px] rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font=extrabold leading-tight text-3xl"></span>
+          <div className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
+            <div className="w-3/4 h-3 rounded-xl bg-violet-600"></div>
+          </div>
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
