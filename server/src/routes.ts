4886c1150abaf5db1de30ce6495037112fd55b6a
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2023-02-05 05:55:20 -0300

    backend - ajustes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -138,18 +138,19 @@ export async function appRoutes(app: FastifyInstance) {
       (
         SELECT
           cast(count(*) as float)
-        FROM days_habits DH, habits H
+        FROM days_habits DH
         WHERE
           DH.day_id = D.id
       ) as completed,
       (
         SELECT
           cast(count(*) as float)
-        FROM habits_weeks_days HWD, habits H
+        FROM habits_weeks_days HWD
+        JOIN habits H
+          ON H.id = HWD.habit_id
         WHERE
           HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
           AND H.created_at <= D.date
-
       ) as amount
       FROM days D
     `
